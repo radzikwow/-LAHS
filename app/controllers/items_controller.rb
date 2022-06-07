9ae63397052d1228242ced9d7e92e1413ae057db
benedetta castagna <benny.castagna@gmail.com>
2022-06-07 15:50:17 +0200

    form + item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,19 @@
 class ItemsController < ApplicationController
-  def edit
-    @item = Item.where(name: "weapon")
+
+  def index
+    @items = Item.all
   end
 
   def update
-    raise
-    @item = Item.where(name: "weapon",)
+    @item = Item.find_by(name: params[:item][:name])
+    @current_level = CurrentLevel.find_by(level: params[:item][:current_level])
+    @item.update(current_level: @current_level)
+
+    if @item.update(current_level: @current_level)
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
 end
