b1f440a03e1a0a8bc2169609b785bf2e2019d67f
Filip Radzikowski <radzikpl@hotmail.com>
2022-06-08 17:26:15 +0200

    i dont know whats happening

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
     @current_level = CurrentLevel.find(params[:current_level])
-    service = CalculationService.new(@current_level)
+    @item = Item.joins(:current_level).where(current_level: @current_level)
+    @materials = @item[0].materials.where(current_level: @current_level)
+    service = CalculationService.new(@current_level, @item)
     @success = service.perform!
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :current_level
   belongs_to :attempt
+  has_many :materials
 end
