fb3bab98a4e3b95b1d2901be3c72b3e6f68d6988
Emre Arduman <earduman2@gmail.com>
2022-06-08 19:05:13 +0300

    add sliders and active img

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @current_level = CurrentLevel.find(params[:current_level])
-    service = CalculationService.new(@current_level)
-    @success = service.perform!
+    # @current_level = CurrentLevel.find(params[:current_level])
+    # service = CalculationService.new(@current_level)
+    # @success = service.perform!
   end
 end
