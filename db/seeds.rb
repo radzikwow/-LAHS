ac15d04f8ee4da8830090e902b354f787bbe613f
azrozza shah <azrozzashah@gmail.com>
2022-06-08 18:08:07 +0800

    items, materials, levels seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,56 @@
-Material.destroy_all
 Item.destroy_all
+Attempt.destroy_all
+Material.destroy_all
 SuccessChance.destroy_all
 CurrentLevel.destroy_all
-Attempt.destroy_all
 p "Destroying all seeds"
 p "All seeds destroyed"
-attempt = Attempt.create!
 p "Creating an attempt"
-current_level = CurrentLevel.create!(level: 12)
-CurrentLevel.create!(level: 15)
+attempt = Attempt.create!
 p "Creating a current level"
+20.times do |i|
+  CurrentLevel.create(level: i)
+end
+current_level = CurrentLevel.where(level: 0).first
+p "Creating success chances"
+SuccessChance.create!(current_level: current_level, base: 60, max: 60)
+SuccessChance.create!(current_level: current_level, base: 45, max: 60)
 SuccessChance.create!(current_level: current_level, base: 30, max: 60)
-p "Creating a success chance"
+SuccessChance.create!(current_level: current_level, base: 15, max: 60)
+SuccessChance.create!(current_level: current_level, base: 10, max: 60)
+SuccessChance.create!(current_level: current_level, base: 5, max: 60)
+SuccessChance.create!(current_level: current_level, base: 3, max: 60)
+p "Creating items"
 item1 = Item.create!(name: "weapon", item_type: "weapon", current_level: current_level, attempt: attempt)
-p "Creating item1 as item1"
-Item.create!(name: "helmet", item_type: "armour", current_level: current_level, attempt: attempt)
-p "Creating item2"
+5.times do
+  Item.create!(name: "helmet", item_type: "armour", current_level: current_level, attempt: attempt)
+end
+p "Creating materials"
+Material.create!(item: item1, name: "shards", amount: 20, current_level: current_level)
+Material.create!(item: item1, name: "gold", amount: 20, current_level: current_level)
+Material.create!(item: item1, name: "silver", amount: 20, current_level: current_level)
 Material.create!(item: item1, name: "leapstone", amount: 20, current_level: current_level)
-p "Creating material"
+Material.create!(item: item1, name: "destruction", amount: 20, current_level: current_level)
+Material.create!(item: item1, name: "guardian", amount: 20, current_level: current_level)
+Material.create!(item: item1, name: "oreha", amount: 20, current_level: current_level)
+p "All instances created!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+p "Creating items"
+item1 = Item.create!(name: "weapon", item_type: "weapon", current_level: current_level, attempt: attempt)
+5.times do
+  Item.create!(name: "helmet", item_type: "armour", current_level: current_level, attempt: attempt)
+end
