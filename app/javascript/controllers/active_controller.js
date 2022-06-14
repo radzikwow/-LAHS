eb52b76eaf02d8645b6d4b1742b7ff540ede910a
benedetta castagna <benny.castagna@gmail.com>
2022-06-14 06:46:42 +0200

    show selected item & active class initial animation

--- app/javascript/controllers/active_controller.js
+++ app/javascript/controllers/active_controller.js
@@ -10,13 +10,15 @@ export default class extends Controller {
 
   select(event){
     // get id of the div
-    console.log(event.originalTarget.parentElement.attributes[0])
-    const selectedItemId = event.originalTarget.parentElement.attributes[0]
+    const selectedItemName = event.originalTarget.parentElement.attributes[0]
     const selectedItem = event.originalTarget.parentElement
 
     // toggle active class on listed item
-    selectedItem.classList.toggle("active")
+    selectedItem.classList.toggle("active-item")
 
-
+    // display selected item on honing position
+    const honingItemImg = this.honedTarget.children[0]
+    const imgTag = `<img class=\"selected_item\" src=\"/assets/items/${selectedItemName.value}.png\">`
+    honingItemImg.outerHTML = imgTag
   }
 }
