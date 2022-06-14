c434fc3c459c0fef15f27f97b0e6a7c440bc9603
benedetta castagna <benny.castagna@gmail.com>
2022-06-14 06:34:33 +0200

    toggle active class on selected item

--- app/javascript/controllers/active_controller.js
+++ app/javascript/controllers/active_controller.js
@@ -2,13 +2,21 @@ import { Controller } from "stimulus"
 import ContextModuleFactory from "webpack/lib/ContextModuleFactory"
 
 export default class extends Controller {
-  static targets = [ "selected" ]
+  static targets = [ "honed" ]
 
   connect() {
     console.log("hello")
   }
 
   select(event){
-    console.log(this.selectedTarget)
+    // get id of the div
+    console.log(event.originalTarget.parentElement.attributes[0])
+    const selectedItemId = event.originalTarget.parentElement.attributes[0]
+    const selectedItem = event.originalTarget.parentElement
+
+    // toggle active class on listed item
+    selectedItem.classList.toggle("active")
+
+
   }
 }
