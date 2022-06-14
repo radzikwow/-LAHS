e5a40ef299bad4d48beb14f823a59db1c634ad0d
benedetta castagna <benny.castagna@gmail.com>
2022-06-14 06:51:57 +0200

    fixed item img

--- app/javascript/controllers/active_controller.js
+++ app/javascript/controllers/active_controller.js
@@ -4,10 +4,6 @@ import ContextModuleFactory from "webpack/lib/ContextModuleFactory"
 export default class extends Controller {
   static targets = [ "honed" ]
 
-  connect() {
-    console.log("hello")
-  }
-
   select(event){
     // get id of the div
     const selectedItemName = event.originalTarget.parentElement.attributes[0]
